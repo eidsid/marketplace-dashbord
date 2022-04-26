ee122689d14ad5e0ad077631ff3bd26fc2472b51
eid sid <eidsiddiedis@gmail.com>
2022-04-26 22:10:56 +0200

    add Container component to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Menu from "./components/menu/Menu";
+import Container from "components/container/Container";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Container />
     </div>
   );
 }
